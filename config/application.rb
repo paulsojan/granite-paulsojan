72090bde111ac157ef5228c9c7ace98d0b45427b
Paul Sojan <paul.eliassojan@gmail.com>
2022-09-01 19:45:08 +0530

    Added linux platform to lockfile

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Granite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
